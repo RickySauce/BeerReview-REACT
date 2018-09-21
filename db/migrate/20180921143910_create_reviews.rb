513c5e8a46eab1a1b3a4ffc5fc0758aff65d9386
learn <eric.desimone@cortland.edu>
2018-09-21 07:53:28 -0700

    migrated user, review and beer tables

--- db/migrate/20180921143910_create_reviews.rb
+++ db/migrate/20180921143910_create_reviews.rb
@@ -1,7 +1,14 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.string :content
+       t.integer :beer_id
+       t.integer :user_id
+       t.float :taste
+       t.float :look
+       t.float :smell
+       t.float :feel
+       t.float :rating
       t.timestamps
     end
   end
--- db/migrate/20180921143916_create_beers.rb
+++ db/migrate/20180921143916_create_beers.rb
@@ -1,7 +1,12 @@
 class CreateBeers < ActiveRecord::Migration[5.2]
   def change
     create_table :beers do |t|
-
+      t.string :name
+      t.string :description
+      t.string :style
+      t.integer :ibu
+      t.float :abv
+      t.float :rating
       t.timestamps
     end
   end
