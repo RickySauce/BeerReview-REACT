04f73f926592eb1992f1d98e0f410d806de407e3
learn <eric.desimone@cortland.edu>
2018-09-21 09:10:18 -0700

    added seed file, seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.create([{
+  name: "gregory",
+  description: "IS OKAY",
+  style: "IPA",
+  abv: 5.2
+},
+{
+  name: "1738",
+  description: "TASTES LIKE FISH",
+  style: "Blonde Ale",
+  abv: 5.0
+},
+{
+  name: "YUNG-SAUCE",
+  description: "TASTES ALSO LIKE FISH",
+  style: "Porter",
+  abv: 7.0
+},
+{
+  name: "RICK ROSS THE SAUCE BOSS",
+  description: "TASTES ALSO, ALSO LIKE FISH",
+  style: "Porter",
+  abv: 8.3
+},
+{
+  name: "JEEZY",
+  description: "RA RA LIKE A DUNGEON DRAGON",
+  style: "IPA",
+  abv: 5.6
+},
+{
+  name: "CHEESE FRIES",
+  description: "TASTES LIKE BEEF",
+  style: "IPA",
+  abv: 5.2
+},
+{
+  name: "Gregory Fitzgerald",
+  description: "SMELLS LIKE BEEF",
+  style: "Stout",
+  abv: 5.7
+},
+{
+  name: "SUHDOOD",
+  description: "WOT'S GOIN ON BIFF",
+  style: "Stout",
+  abv: 6.4
+},
+{
+  name: "DOOD SUH",
+  description: "CHEESE FRIES",
+  style: "Blonde Ale",
+  abv: 4.5
+},
+{
+  name: "OUT HERE IN AMISH",
+  description: "SMOKIN' BIG DOINKS IN AMISH",
+  style: "Porter",
+  abv: 6.4
+}
+])
