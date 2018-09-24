392fae88b78d3928f8a215427826037bd4f02fc5
learn <eric.desimone@cortland.edu>
2018-09-24 06:04:38 -0700

    further improved the functionality of user Sign Up form, added in renderErrors() method which displays errors preventing a user creating a new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       render :json => @user, status: 201
    else
--- client/src/user/SignUpForm.js
+++ client/src/user/SignUpForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class SignUpForm extends Component {
 
   state = {
+    errors: [],
     username: '',
     email: '',
     password: '',
@@ -11,7 +12,7 @@ class SignUpForm extends Component {
 
   handleChange = (event) => {
     this.setState({
-      [event.target.id]: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
@@ -28,43 +29,59 @@ class SignUpForm extends Component {
 })
     .then(res => res.json())
     .then(json => {
-      console.log(this.state)
-      let state = json.errors
-       this.setState({state})
-       console.log(this.state)
+      const errors = json.errors
+      this.setState({errors})
     })
+  }
+
+  renderErrors = () => {
+    const errors = []
+    if (this.state.errors != false) {
+      for (var el in this.state.errors){
+        errors.push(`${el}: ${this.state.errors[el].join(', ')}`)
+      }
+      return (
+        <div>
+        The following errors prevented the creation of the account:
+        <br/><br/>
+        {errors.map(el => <li>{el}</li>)}
+        <br/><br/>
+        </div>
+      )
+    }
   }
 
   render(){
     return(
       <div>
         <h3>Sign Up Here!</h3>
+        {this.renderErrors()}
         <form onSubmit={this.handleSubmit}>
         <label>Username: </label>
         <input
         id="username"
-        name="user[username]"
+        name="username"
         type="text"
         value={this.state.username}
         onChange={this.handleChange}/><br/><br/>
         <label>Email: </label>
         <input
         id="email"
-        name="user[email]"
+        name="email"
         type="email"
         value={this.state.email}
         onChange={this.handleChange}/><br/><br/>
         <label>Password: </label>
         <input
         id="password"
-        name="user[password]"
+        name="password"
         type="password"
         value={this.state.password}
         onChange={this.handleChange}/><br/><br/>
         <label>Password Confirmation: </label>
         <input
         id="password_confirmation"
-        name="user[password_confirmation]"
+        name="password_confirmation"
         type="password"
         value={this.state.password_confirmation}
         onChange={this.handleChange}/><br/><br/>
