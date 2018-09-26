fe08f75e407696a9eee18a54d1e5d1574305c947
learn <eric.desimone@cortland.edu>
2018-09-26 15:15:16 -0700

    refactored nav bar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -5,44 +5,71 @@ import { connect } from 'react-redux';
 
 
 const NavBar = (props) => {
-
+  const renderLinks = () => {
+    if (props.user != false){
+      return (
+        <div>
+        <NavLink
+           style={{ margin: "20px"}}
+           to="/home"
+         >Home</NavLink>
+        <NavLink
+           style={{ margin: "20px"}}
+           to="/userprofile"
+         >User Profile</NavLink>
+         <NavLink
+            style={{ margin: "20px"}}
+            onClick={props.logOut}
+            to="/login"
+          >Log Out
+          </NavLink>
+          <NavLink
+             style={{ margin: "20px"}}
+             to="/reviews"
+           >Reviews
+           </NavLink>
+           <NavLink
+              style={{ margin: "20px"}}
+              to="/beers"
+            >Beers
+            </NavLink>
+          </div>
+      )
+    } else {
+      return (
+        <div>
+        <NavLink
+           style={{ margin: "20px"}}
+           to="/home"
+         >Home</NavLink>
+        <NavLink
+           style={{ margin: "20px"}}
+           to="/signup"
+         >Sign Up
+         </NavLink>
+         <NavLink
+            style={{ margin: "20px"}}
+            to="/login"
+          >Log In
+          </NavLink>
+          <NavLink
+             style={{ margin: "20px"}}
+             to="/reviews"
+           >Reviews
+           </NavLink>
+           <NavLink
+              style={{ margin: "20px"}}
+              to="/beers"
+            >Beers
+            </NavLink>
+          </div>
+      )
+    }
+  }
 
   return (
   <div>
-  <NavLink
-     style={{ margin: "20px"}}
-     to="/home"
-   >Home</NavLink>
-   <NavLink
-      style={{ margin: "20px"}}
-      to="/userprofile"
-    >Home</NavLink>
-   <NavLink
-      style={{ margin: "20px"}}
-      to="/signup"
-    >Sign Up
-    </NavLink>
-    <NavLink
-       style={{ margin: "20px"}}
-       to="/login"
-     >Log In
-     </NavLink>
-     <NavLink
-        style={{ margin: "20px"}}
-        onClick={props.logOut}
-        to="/login"
-      >Log Out
-      </NavLink>
-      <NavLink
-         style={{ margin: "20px"}}
-         to="/reviews"
-       >Reviews
-       </NavLink><span></span>
-       <NavLink
-          style={{ margin: "20px"}}
-          to="/beers"
-        >Beers
-        </NavLink>
+      {renderLinks()}
   </div>
 )
 }
@@ -52,4 +79,11 @@ const NavBar = (props) => {
   })
 
 
-export default connect(null, mapDispatchToProps)(NavBar);
+  const mapStateToProps = (state) => {
+    return  {
+      user: state.user.user
+    }
+  }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
